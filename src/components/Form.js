7e957e2e93fb61f98c611beaadd1bf5acfd8a2e2
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-05 12:32:23 +0300

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,7 @@ class Form extends Component {
     ...DEFAULT_STATE
   }
 
-  handleSubmit() {
+  handleSubmit = event => {
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
@@ -26,10 +26,12 @@ class Form extends Component {
     })
   }
 
-  handleChange() {
-    const itemType = event.target.name
-    const item = event.target.value
-
+  handleChange = event => {
+    const target = event.target;
+    const itemType = target.name
+    const item = target.value
+    console.log(`itemtype: ${itemType}`)
+    console.log(`item: ${item}`)
     !this.state[`${itemType}`].includes(item) ?
       this.setState({
         [itemType]: this.state[`${itemType}`].concat(item)
@@ -49,22 +51,22 @@ class Form extends Component {
         <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
           <ProteinForm
             protein={ this.state.protein }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <FillingForm
             fillings={ this.state.fillings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <ToppingForm
             toppings={ this.state.toppings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <SideForm
             sides={ this.state.sides }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <br />
