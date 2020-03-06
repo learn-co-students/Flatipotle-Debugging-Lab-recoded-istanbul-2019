e7af1214eca0e1f517311cc6970931fa2faf7e11
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-03-06 20:21:06 +0300

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,17 +16,16 @@ class Form extends Component {
     ...DEFAULT_STATE
   }
 
-  handleSubmit() {
+  handleSubmit = (event) => {
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
-
     this.setState({
       ...DEFAULT_STATE
     })
   }
 
-  handleChange() {
+  handleChange = (event) =>{
     const itemType = event.target.name
     const item = event.target.value
 
